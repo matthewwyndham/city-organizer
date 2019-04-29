d807ef47e032d3b00a3ffd0a0135cfa10813c32c
Matt Wyndham <matthewwyndham@gmail.com>
2019-04-29 11:30:53 -0600

    added custom file reader, reorganized and cleaned up code

--- main.js
+++ main.js
@@ -4,58 +4,33 @@ const {
     prettyprint
 } = require('./utils')
 const { performance } = require('perf_hooks');
-var t0, t1;
 
-t0 = performance.now();
+const fs = require('fs');
+const d3v = require('d3-dsv');
+const path = require('path');
 
-/* I changed this code to allow command line arguments, and to have dynamic filenames */
-// command line syntax: specify file to read as second, add a third arugment to print debugs
-
+/******************/
+/* defualt values */
+/******************/
+var t0, t1, csvData, regions;
 var filename = 'cities.csv';
 var debug = false;
-
-// pass in a filename to process.argv
-if (process.argv[2] === undefined || !process.argv[2].includes(".csv")) {
-    console.log("No file specified, using 'cities.csv'");
-} 
-else {
-    filename = process.argv[2];
-}
-
-// debug mode
-if (process.argv[3] !== undefined) {debug = true;}
-
-console.log("Extracting data from " + filename + " ...");
-var csvData = read(filename);
-
-/* --- Your awesome code here --- */
-// debug
-// console.log(csvData);
-
-console.log("Creating object that groups cities by state and region ...")
-
 var countCities = 0;
+const regionOrder = ['Pacific', 'Mountain', 'Midwest', 'South', 'Northeast']; // keep track of order of regions (west to east) 
 
-// keep track of order of regions (west to east)
-const regionOrder = ['Pacific', 'Mountain', 'Midwest', 'South', 'Northeast'];
-
-// Convert the csv of each city to a nice list organized by
-// region, state, then city
-var regions = csvData.reduce(regionReducer, []);
-
-// each region, state, and city is an object
-// (reg = reduced 'region' array, city = current 'city' from csv)
+/**********************************/
+/* city-csv to region-md reducer! */
+/**********************************/
 function regionReducer(reg, city) {
+    // reg is 'region', meaning the collection of cities organized into groups in states, in regions
     var findRegion = reg.find(e => e.name === city['REGION']);
     // check if region does not exist
     if (findRegion === undefined) {
         // new region object (string:name, array:states)
         reg.push({name: city['REGION'], states: []});
-        reg.sort((a,b) => regionOrder.findIndex(e => e === a.name) - regionOrder.findIndex(e => e === b.name)); // sorted by defined order
+        reg.sort((a,b) => regionOrder.findIndex(e => e === a.name) - regionOrder.findIndex(e => e === b.name)); // sorted by globally defined order
 
-        if (debug) {
-            process.stdout.write("[R]");
-        }
+        if (debug) {process.stdout.write("[R]");}
 
         findRegion = reg.find(e => e.name === city['REGION']);
     }
@@ -65,11 +40,9 @@ function regionReducer(reg, city) {
     if (findStates === undefined) {
         // new state object (string:name, array:cities)
         findRegion.states.push({name: city['STATE'], cities: []});
-        findRegion.states.sort((a,b) => a.name.localeCompare(b.name)); // alphabetical
+        findRegion.states.sort((a,b) => a.name.localeCompare(b.name)); // alphabetical by name
 
-        if (debug) {
-            process.stdout.write("S");
-        }
+        if (debug) {process.stdout.write("S");}
 
         findStates = findRegion.states.find(e => e.name === city['STATE']);
     }
@@ -90,29 +63,47 @@ function regionReducer(reg, city) {
 
     return reg; // this is an object so this is a reference and not a copy
 }
+/* start of timing */
+/*-----------------*/
+t0 = performance.now(); // for timing how long this process runs
 
-if (debug) {
-    console.log("\ntotal cities:", countCities);
+/**********************************/
+/* command line argument handling */
+/**********************************/
+// command line syntax: second argument will specify file to read
+if (process.argv[2] === undefined || !process.argv[2].includes(".csv")) {
+    console.log("No file specified, using", filename);
+} 
+else {
+    filename = process.argv[2];
 }
 
-console.log("Writing data to regions-" + filename.substring(0, filename.length - 4) + ".md ...");
+// add a third arugment to print debug info (I suggest: ' debug')
+if (process.argv[3] !== undefined) {debug = true;}
 
-// remove '.csv' from the name
+/*************/
+/* read file */
+/*************/
+console.log("Extracting data from " + filename + " ...");
+csvData = d3v.csvParse(fs.readFileSync(filename, 'utf8'));
+
+/**************/
+/* parse file */
+/**************/
+/* convert the csv to a nice format that is easy to print as markdown */
+console.log("Creating object that groups cities by state and region ...")
+regions = csvData.reduce(regionReducer, []);
+
+if (debug) {console.log("\ntotal cities:", countCities);}
+
+/******************/
+/* write new file */
+/******************/
+// substring to remove '.csv' from the name
+console.log("Writing data to regions-" + filename.substring(0, filename.length - 4) + ".md ...");
 write('regions-' + filename.substring(0, filename.length - 4) + '.md', regions);
 
+/* end of timing */
+/*---------------*/
 t1 = performance.now();
-
-if (debug) {
-    console.log(`runtime: ${((t1-t0) / 1000).toPrecision(4)} sec`);
-}
-
-// debug
-// console.log("\nData");
-// console.log('Region'.padStart(10), 'State'.padStart(15), 'City'.padStart(20), 'Population'.padStart(15));
-// for (let r of regions) {
-//     for (let s of r.states) {
-//         for (let c of s.cities) {
-//             console.log(r.name.padStart(10), s.name.padStart(15), c.name.padStart(20), c.population.toString().padStart(15));
-//         }
-//     }
-// }
+console.log(`runtime: ${((t1-t0) / 1000).toPrecision(4)} sec`);
